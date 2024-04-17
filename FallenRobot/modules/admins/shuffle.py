aca6fd95567d609cb76f2cdfa214f311dbb1a93a
R7-suii <144661479+R7-suii@users.noreply.github.com>
2024-04-17 20:03:22 +0600

    Update shuffle.py

--- FallenRobot/modules/admins/shuffle.py
+++ FallenRobot/modules/admins/shuffle.py
@@ -3,10 +3,10 @@ import random
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import db
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from FallenRobot import app
+from FallenRobot.misc import db
+from FallenRobot.utils.decorators import AdminRightsCheck
+from FallenRobot.utils.inline import close_markup
 from config import BANNED_USERS
 
 
