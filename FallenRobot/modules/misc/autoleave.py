36bdccbdc79325edd71a0c21067b0ff51a6a20e6
R7-suii <144661479+R7-suii@users.noreply.github.com>
2024-04-17 21:05:22 +0600

    Update autoleave.py

--- FallenRobot/modules/misc/autoleave.py
+++ FallenRobot/modules/misc/autoleave.py
@@ -4,9 +4,9 @@ from datetime import datetime
 from pyrogram.enums import ChatType
 
 import config
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony, autoend
-from AnonXMusic.utils.database import get_client, is_active_chat, is_autoend
+from FallenRobot import app
+from FallenRobot.core.call import Anony, autoend
+from FallenRobot.utils.database import get_client, is_active_chat, is_autoend
 
 
 async def auto_leave():
