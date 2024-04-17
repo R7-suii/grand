65efd49b1e7a057de9ced8761026d094f1fe887b
R7-suii <144661479+R7-suii@users.noreply.github.com>
2024-04-17 20:07:43 +0600

    Update seek.py

--- FallenRobot/modules/admins/seek.py
+++ FallenRobot/modules/admins/seek.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import YouTube, app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.misc import db
-from AnonXMusic.utils import AdminRightsCheck, seconds_to_min
-from AnonXMusic.utils.inline import close_markup
+from FallenRobot import YouTube, app
+from FallenRobot.core.call import Anony
+from FallenRobot.misc import db
+from FallenRobot.utils import AdminRightsCheck, seconds_to_min
+from FallenRobot.utils.inline import close_markup
 from config import BANNED_USERS
 
 
