8820fdc16e69b589a14b1a0494481a518cc866f5
R7-suii <144661479+R7-suii@users.noreply.github.com>
2024-04-18 03:13:05 +0600

    Update logger.py

--- FallenRobot/modules/sudo/logger.py
+++ FallenRobot/modules/sudo/logger.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import add_off, add_on
-from AnonXMusic.utils.decorators.language import language
+from FallenRobot import app
+from FallenRobot.misc import SUDOERS
+from FallenRobot.utils.database import add_off, add_on
+from FallenRobot.utils.decorators.language import language
 
 
 @app.on_message(filters.command(["logger"]) & SUDOERS)
