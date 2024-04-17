cb621be5179647a8e36851bf3ae104237d292ce4
R7-suii <144661479+R7-suii@users.noreply.github.com>
2024-04-17 21:08:42 +0600

    Update watcher.py

--- FallenRobot/modules/misc/watcher.py
+++ FallenRobot/modules/misc/watcher.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
+from FallenRobot import app
+from FallenRobot.core.call import Anony
 
 welcome = 20
 close = 30
