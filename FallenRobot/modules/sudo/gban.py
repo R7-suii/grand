5e639d8526e262296e188aacc001350170a40110
R7-suii <144661479+R7-suii@users.noreply.github.com>
2024-04-18 03:11:18 +0600

    Update gban.py

--- FallenRobot/modules/sudo/gban.py
+++ FallenRobot/modules/sudo/gban.py
@@ -4,10 +4,10 @@ from pyrogram import filters
 from pyrogram.errors import FloodWait
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils import get_readable_time
-from AnonXMusic.utils.database import (
+from FallenRobot import app
+from FallenRobot.misc import SUDOERS
+from FallenRobot.utils import get_readable_time
+from FallenRobot.utils.database import (
     add_banned_user,
     get_banned_count,
     get_banned_users,
@@ -15,8 +15,8 @@ from AnonXMusic.utils.database import (
     is_banned_user,
     remove_banned_user,
 )
-from AnonXMusic.utils.decorators.language import language
-from AnonXMusic.utils.extraction import extract_user
+from FallenRobot.utils.decorators.language import language
+from FallenRobot.utils.extraction import extract_user
 from config import BANNED_USERS
 
 
