73efab643b0eada296155881eabebfac8003f151
R7-suii <144661479+R7-suii@users.noreply.github.com>
2024-04-17 19:54:57 +0600

    Update auth.py

--- FallenRobot/modules/admins/auth.py
+++ FallenRobot/modules/admins/auth.py
@@ -1,16 +1,16 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.utils import extract_user, int_to_alpha
-from AnonXMusic.utils.database import (
+from FallenRobot import app
+from FallenRobot.utils import extract_user, int_to_alpha
+from FallenRobot.utils.database import (
     delete_authuser,
     get_authuser,
     get_authuser_names,
     save_authuser,
 )
-from AnonXMusic.utils.decorators import AdminActual, language
-from AnonXMusic.utils.inline import close_markup
+from FallenRobot.utils.decorators import AdminActual, language
+from FallenRobot.utils.inline import close_markup
 from config import BANNED_USERS, adminlist
 
 
